85e612087198a1824f953e34a96f5266d973d67e
Ulquiorra <rodiontrig@gmail.com>
2024-06-02 01:40:27 +0300

    Change origin

--- src/main/java/com/example/chatback/configuration/WebConfig.java
+++ src/main/java/com/example/chatback/configuration/WebConfig.java
@@ -24,7 +24,7 @@ public class WebConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("ulquiorrashif-chatfrontend-5665.twc1.net");
+        config.addAllowedOrigin("https://ulquiorrashif-chatfrontend-5665.twc1.net");
         config.setAllowedHeaders(Arrays.asList(
                 HttpHeaders.AUTHORIZATION,
                 HttpHeaders.CONTENT_TYPE,
